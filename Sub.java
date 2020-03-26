dd11feba59d3250310c071b42ac2bd7e893b34fc
panditwalde <pandit.r@techchefs.in>
2020-03-26 17:11:12 +0530

    add subtraction

---
+++ Sub.java
@@ -0,0 +1,16 @@
+
+public class Sub
+{
+
+   public static void main(String x[]){
+
+
+        int num1=10;
+        int num2=20;
+
+      System..out.println("Addtion two number"+(num1-num2));
+
+
+}
+
+}
