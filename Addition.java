19c738ed972d5dab5e13dbd83fb7d9b4bd6dbaa9
panditwalde <pandit.r@techchefs.in>
2020-03-26 17:06:05 +0530

    Addition

---
+++ Addition.java
@@ -0,0 +1,16 @@
+
+public class HelloDemo
+{
+
+   public static void main(String x[]){
+
+
+        int num1=10;
+        int num2=20;
+
+      System..out.println("Addtion two number"+(num1+num2));
+
+
+}
+
+}
